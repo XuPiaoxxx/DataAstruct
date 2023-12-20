4c36483f525aee918d1f6f4b27254f953c3b5a13
Xupaioxxx <2693798300@qq.com>
2023-12-20 15:23:25 +0800

    Leetcode84

--- src/CodeFly/AL000LeetCode.java
+++ src/CodeFly/AL000LeetCode.java
@@ -1,46 +1,26 @@
 package CodeFly;
 
-/**
- * 最大二叉树-LeetCode:654-代码随想录
- * 2023.09.15/22.18
+import java.util.*;
 
- 题目:
-
- TIPS:
-*/
 
 public class AL000LeetCode {
-    public static void main(String[] args) {
-        canPlaceFlowers(new int[]{1,0,0,0,1},1);
+
+    static int r = 0;
+    public static void main(String[] args) throws InterruptedException {
+        test1();
     }
-    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int res=0;
-        if(flowerbed.length<3){
-            for(int i:flowerbed){
-                if(i==1) res++;
+    private static void test1() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            return res>0?false:true;
-        }
-        int pre=0,next=flowerbed[1];
-        for(int i=0;i<flowerbed.length;i++){
-            if(flowerbed[i]==0&&pre==0&&next==0){
-                res++;
-                flowerbed[i]=1;
-                pre=flowerbed[i];
-                if(i==flowerbed.length-2){
-                    next=0;
-                }else{
-                    next=flowerbed[i+2];
-                }
-            }else{
-                pre=flowerbed[i];
-                if(i==flowerbed.length-2){
-                    next=0;
-                }else{
-                    next=flowerbed[i+2];
-                }
-            }
-        }
-        return res>=n?true:false;
+            r = 10;
+        });
+        t1.start();
+//        t1.join();//不等待线程执行结束，输出的10
+        System.out.println(r);
     }
 }
+
