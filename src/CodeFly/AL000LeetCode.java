9f282767db47ab73af0e2f20ee86a2694f9c8b6c
Xupaioxxx <2693798300@qq.com>
2024-01-14 13:30:47 +0800

    2024/1/14

--- src/CodeFly/AL000LeetCode.java
+++ src/CodeFly/AL000LeetCode.java
@@ -5,22 +5,18 @@ import java.util.*;
 
 public class AL000LeetCode {
 
-    static int r = 0;
     public static void main(String[] args) throws InterruptedException {
-        test1();
-    }
-    private static void test1() throws InterruptedException {
-        Thread t1 = new Thread(() -> {
+        Thread t1 = new Thread(()->{
             try {
-                Thread.sleep(1000);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            r = 10;
-        });
+        }, "t1");
         t1.start();
-//        t1.join();//不等待线程执行结束，输出的10
-        System.out.println(r);
+        Thread.sleep(500);
+        t1.interrupt();
+        System.out.println(" 打断状态: {}" + t1.isInterrupted());// 打断状态: {}false
     }
 }
 
